dfca750d2921aba932706ee4c54a6ef909007269
HenzouGT <htsunosse@gmail.com>
2026-06-22 10:50:03 -0400

    feat: adicionando tratamento de erro na função deactivate

--- src/modules/user/repositories/prisma-user.repository.ts
+++ src/modules/user/repositories/prisma-user.repository.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { Role, User } from '@prisma/client';
+import { Prisma, Role, User } from '@prisma/client';
 import { PrismaService } from '../../../infra/prisma/prisma.service';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { UserRepository } from './user.repository';
+import { ApplicationError } from '../../../common/errors/application.error';
 
 @Injectable()
 export class PrismaUserRepository implements UserRepository {
@@ -48,12 +49,26 @@ export class PrismaUserRepository implements UserRepository {
     });
   }
 
-  deactivate(userId: string): Promise<User> {
-    return this.prisma.user.update({
-      where: { userId },
-      data: {
-        desativado: true,
-      },
-    });
+  async deactivate(userId: string): Promise<User> {
+    try {
+      return await this.prisma.user.update({
+        where: { userId },
+        data: {
+          desativado: true,
+        },
+      });
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2025') {
+          throw new ApplicationError(404, 'Usuário não encontrado', error);
+        }
+        throw new ApplicationError(400, 'Erro ao atualizar usuário', error);
+      }
+      throw new ApplicationError(
+        500,
+        'Erro inesperado ao desativar usuário',
+        error,
+      );
+    }
   }
 }
